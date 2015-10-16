5705f1db3230754f8fe400aa506c0dbf693a6cee
Balakrishnan Balasubramanian <balakrishnan.b@chronus.com>
2015-10-16 15:38:35 +0530

    created new rails project with cmd line rails new blog --database=mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ec4486a73225d557193a109cbdfe3e6c6a8bb231efa271f3dab017acecddaaee9c9ab4c297d1368fff1263fa599d3cca7501b814867b34779fb5f27ef70e4e45'
